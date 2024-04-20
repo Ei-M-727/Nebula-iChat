0bd1a0f4eedfc393ee5ee0c4de99270c4c315260
mei <2789191478@qq.com>
2024-04-20 21:46:27 +0800

    update

---
+++ Client/src/api/url.js
@@ -0,0 +1,8 @@
+const base = {
+  user: 'http://123.60.59.12:3000/api/user',
+  friendly: 'http://123.60.59.12:3000/api/friendly',
+  upload: 'http://123.60.59.12:3000/api/upload',
+  group: 'http://123.60.59.12:3000/api/group'
+};
+
+export default base;
